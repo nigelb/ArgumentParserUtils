e5a29f13885987ca20487f9e87c7df9f831bf6bf
NigelB <nigel.blair@gmail.com>
2024-05-19 19:20:23 +1000

    Updated and extra helpers

---
+++ argparseutils/helpers/mqtt.py
@@ -0,0 +1,103 @@
+# ArgumentParserUtils provides Utilities and helpers for Python's
+# ArgumentParser.
+#
+# Copyright 2024 NigelB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ssl
+
+import paho.mqtt.client as mqtt_client
+from paho.mqtt.enums import CallbackAPIVersion
+
+from argparseutils.helpers.utils import CliShardWrapper, __get_env__, get_shard_values, fix_formatter_class, get_args, \
+    get_shard_registry
+
+
+class MQTTClientHelper:
+
+    @classmethod
+    def add_parser_options(cls, parser, mqtt_client_id, shard=""):
+        cli_shard, help_shard = get_shard_values(shard)
+        fix_formatter_class(parser)
+        get_shard_registry().register_shard(cls, shard)
+
+        parser.add_argument("--%smqtt-host" % cli_shard, default=__get_env__("MQTT_HOST", "localhost", shard=shard),
+                            help="The MQTT server hostname to connect to%s." % help_shard)
+        parser.add_argument("--%smqtt-port" % cli_shard, default=__get_env__("MQTT_PORT", 1883, type=int, shard=shard),
+                            type=int, help="The MQTT Server port on connect to%s." % help_shard)
+        parser.add_argument("--%smqtt-ssl" % cli_shard, action="store_true",
+                            help="Use SSL when connecting to the MQTT server%s." % help_shard)
+        parser.add_argument("--%smqtt-client-id" % cli_shard,
+                            default=__get_env__("MQTT_CLIENT_ID", mqtt_client_id, shard=shard),
+                            help="The MQTT Client Id to use on the connection%s." % help_shard)
+        parser.add_argument("--%smqtt-keepalive" % cli_shard,
+                            default=__get_env__("MQTT_KEEPALIVE", 60, type=int, shard=shard), type=int,
+                            help="The MQTT connection keepalive%s." % help_shard)
+        parser.add_argument("--%smqtt-username" % cli_shard, default=__get_env__("MQTT_USERNAME", None, shard=shard),
+                            help="The MQTT Username to connect with%s." % help_shard)
+        parser.add_argument("--%smqtt-password" % cli_shard, default=__get_env__("MQTT_PASSWORD", None, shard=shard),
+                            help="The MQTT Password to connect with%s." % help_shard)
+        parser.add_argument("--%smqtt-transport" % cli_shard, default=__get_env__("MQTT_TRANSPORT", 'tcp', shard=shard),
+                            choices=['tcp', 'websockets'],
+                            help="The MQTT Transport to use with the connection%s." % help_shard)
+        parser.add_argument("--%smqtt-clean-session" % cli_shard, default=True, type=bool,
+                            help="If False, the client is a persistent client and subscription information and queued " \
+                                 "messages will be retained when the client disconnects%s." % help_shard)
+        parser.add_argument("--%smqtt-ws-path" % cli_shard, default=__get_env__("MQTT_WS_PATH", '/mqtt/', shard=shard),
+                            help="The MQTT Websocket path%s." % help_shard)
+
+    @classmethod
+    def validate_args(cls, args, shard=""):
+        return True
+
+    @classmethod
+    def create_client(cls, args, shard=""):
+        args = get_args(args, shard)
+
+        client = mqtt_client.Client(
+            client_id=args.mqtt_client_id,
+            clean_session=args.mqtt_clean_session,
+            transport=args.mqtt_transport,
+            callback_api_version=CallbackAPIVersion.VERSION2
+        )
+        client.args = args
+        client.ws_set_options(path=args.mqtt_ws_path)
+        if args.mqtt_ssl:
+            client.tls_set(
+                ca_certs=None,
+                certfile=None,
+                keyfile=None,
+                cert_reqs=ssl.CERT_REQUIRED,
+                tls_version=ssl.PROTOCOL_TLS,
+                ciphers=None)
+
+        if args.mqtt_username is not None:
+            client.username_pw_set(args.mqtt_username, args.mqtt_password)
+
+        return client
+
+    @classmethod
+    def connect(cls, client):
+        args = client.args
+        client.connect(args.mqtt_host, port=args.mqtt_port, keepalive=args.mqtt_keepalive)
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser("MQTT Test")
+    MQTTClientHelper.add_parser_options(parser, "test", shard="input")
+
+    args = parser.parse_args()
+    input_port = MQTTClientHelper.create_client(args, shard="input")
--- argparseutils/helpers/utils.py
+++ argparseutils/helpers/utils.py
@@ -16,9 +16,14 @@
 # limitations under the License.
 
 import os
+import sys
+from collections import defaultdict
+from functools import lru_cache
+from typing import Any
+from argparse import ArgumentDefaultsHelpFormatter
 
 
-def __get_env__(var_name: str, default: object, type: type = str, env=None) -> object:
+def __get_env__(var_name: str, default: object, type: type = str, shard: str = "", env=None) -> object:
     """
     __get_env__ Looks up the value of the field `var_name` firstly in `env` secondly in the environmental
     variables then if`var_name` it is still not found returns the `default` value
@@ -29,6 +34,8 @@ def __get_env__(var_name: str, default: object, type: type = str, env=None) -> o
     :param env: The dictionary to look for `var_name` in
     :return: The found value or `default`
     """
+    if len(shard.strip()) > 0:
+        var_name = "%s_%s" % (shard.upper(), var_name.upper())
     if env is not None and var_name in env:
         return type(env[var_name].value)
     elif var_name in os.environ:
@@ -37,3 +44,72 @@ def __get_env__(var_name: str, default: object, type: type = str, env=None) -> o
         return type(default)
 
     return None
+
+
+class CliShardWrapper:
+    def __init__(self, args, shard):
+        object.__setattr__(self, 'args', args)
+        object.__setattr__(self, 'shard', shard)
+
+    def __getattribute__(self, name: str) -> Any:
+        args = object.__getattribute__(self, 'args')
+        partition = object.__getattribute__(self, 'shard')
+        _name_ = "%s_%s" % (partition, name)
+        return args.__getattribute__(_name_)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        args = object.__getattribute__(self, 'args')
+        partition = object.__getattribute__(self, 'shard')
+        _name_ = "%s_%s" % (partition, name)
+        args.__setattr__(_name_, value)
+
+
+def get_shard_values(shard_name):
+    cli_shard = ""
+    help_shard = ""
+    if len(shard_name) > 0:
+        cli_shard = f"{shard_name}-"
+        help_shard = f" [{shard_name}] "
+    return cli_shard, help_shard
+
+
+def fix_formatter_class(parser, formatter=ArgumentDefaultsHelpFormatter):
+    if not isinstance(parser.formatter_class, ArgumentDefaultsHelpFormatter):
+        parser.formatter_class = ArgumentDefaultsHelpFormatter
+
+
+def get_args(args, shard):
+    if len(shard) > 0:
+        return CliShardWrapper(args, shard)
+    return args
+
+
+class ShardRegistry:
+    def __init__(self):
+        self.shards = defaultdict(list)
+        self.invalid_shard_handler = lambda helper_class, shard: self.__default_invalid_shard_handler(helper_class,
+                                                                                                      shard)
+
+    def __default_invalid_shard_handler(self, helper_class, shard):
+        print(f"Shard {shard} has not been registered for helper {helper_class.__name__}", file=sys.stderr)
+        sys.exit(5)
+
+    def register_shard(self, helper_class, shard):
+        helper_shards = self.shards[helper_class.__name__]
+        if shard not in helper_shards:
+            helper_shards.append(shard)
+
+    def registered_shards(self, helper_class):
+        return self.shards[helper_class.__name__]
+
+    def validate_shard(self, helper_class, shard):
+        if shard not in self.registered_shards(helper_class):
+            self.invalid_shard_handler(helper_class, shard)
+
+    def set_invalid_shard_handler(self, invalid_shard_handler):
+        self.invalid_shard_handler = invalid_shard_handler
+
+
+@lru_cache
+def get_shard_registry():
+    return ShardRegistry()
