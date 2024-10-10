8156f031f99ae4bef3e5dbfd6e2479e1a2e8bcdf
nigelb <nigel.blair@gmail.com>
2024-10-10 21:35:24 +1000

    Add kwargs and fix issues with Serial Helper
    All of the default Serial options SerialHelper.add_parser_options
    can now be overridden by passing in as kwargs to the
    SerialHelper.add_parser_options.
    
    Fixed the issue of baudrate now being passed to the Serial object
    on creation.

--- argparseutils/helpers/serialport.py
+++ argparseutils/helpers/serialport.py
@@ -19,6 +19,7 @@ import os
 from decimal import Decimal
 
 from serial import Serial
+from serial.tools import list_ports
 from serial.serialutil import FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS, PARITY_NONE, PARITY_EVEN, PARITY_ODD, \
     PARITY_MARK, PARITY_SPACE, STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO
 
@@ -35,42 +36,48 @@ class SerialHelper:
         "Space": PARITY_SPACE
     }
 
+    stopbit_map = {
+        str(STOPBITS_ONE): STOPBITS_ONE,
+        str(STOPBITS_ONE_POINT_FIVE): STOPBITS_ONE_POINT_FIVE,
+        str(STOPBITS_TWO): STOPBITS_TWO
+    }
+
     @classmethod
-    def add_parser_options(cls, parser, shard=""):
+    def add_parser_options(cls, parser, shard="", **kwargs):
         cli_shard, help_shard = get_shard_values(shard)
         fix_formatter_class(parser)
         get_shard_registry().register_shard(cls, shard)
         add_env_parser_options(parser)
 
-        parser.add_argument(f"--{cli_shard}port", default=__get_env__("PORT", "localhost", shard=shard),
+        parser.add_argument(f"--{cli_shard}port", 
+                            default=__get_env__("PORT", kwargs.get("port", list_ports.comports()[0].device), shard=shard),
                             help=f"The Serial port to connect to. {help_shard}")
-        parser.add_argument(f"--{cli_shard}baudrate", default=__get_env__("BAUDRATE", 9600, shard=shard), type=int,
+        parser.add_argument(f"--{cli_shard}baudrate", default=__get_env__("BAUDRATE", kwargs.get("baudrate", 9600), shard=shard), type=int,
                             help=f"The Serial port baudrate to use. {help_shard}")
-        parser.add_argument(f"--{cli_shard}bytesize", default=__get_env__("BYTESIZE", EIGHTBITS, shard=shard),
+        parser.add_argument(f"--{cli_shard}bytesize", default=__get_env__("BYTESIZE", kwargs.get("bytesize", EIGHTBITS), shard=shard),
                             choices=[FIVEBITS, SIXBITS, SEVENBITS, EIGHTBITS], type=int,
                             help=f"The number of bits for each byte. {help_shard}")
-        parser.add_argument(f"--{cli_shard}parity", default=__get_env__("PARITY", "None", shard=shard),
+        parser.add_argument(f"--{cli_shard}parity", default=__get_env__("PARITY", kwargs.get("parity", "None"), shard=shard),
                             choices=cls.parity_map.keys(), help=f"The parity algorithm to use. {help_shard}")
-        parser.add_argument(f"--{cli_shard}stopbits", default=__get_env__("STOPBITS", STOPBITS_ONE, shard=shard),
-                            choices=[STOPBITS_ONE, STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO], type=Decimal,
+        parser.add_argument(f"--{cli_shard}stopbits", default=__get_env__("STOPBITS", kwargs.get("stopbits", str(STOPBITS_ONE)), shard=shard),
+                            choices=cls.stopbit_map.keys(),
                             help=f"The number of stop bits to use. {help_shard}")
-        parser.add_argument(f"--{cli_shard}timeout", default=__get_env__("TIMEOUT", None, shard=shard), type=Decimal,
+        parser.add_argument(f"--{cli_shard}timeout", default=__get_env__("TIMEOUT", kwargs.get("timeout", None), shard=shard), type=Decimal,
                             help=f"The read timeout to use (seconds). {help_shard}")
-        parser.add_argument(f"--{cli_shard}xonxoff", default=__get_env__("XONXOFF", False, shard=shard), type=bool,
+        parser.add_argument(f"--{cli_shard}xonxoff", default=__get_env__("XONXOFF", kwargs.get("xonxoff", False), shard=shard), type=bool,
                             choices=[True, False], help=f"Use software flow control. {help_shard}")
-        parser.add_argument(f"--{cli_shard}rtscts", default=__get_env__("RTSCTS", False, shard=shard), type=bool,
+        parser.add_argument(f"--{cli_shard}rtscts", default=__get_env__("RTSCTS", kwargs.get("rtscts", False), shard=shard), type=bool,
                             choices=[True, False], help=f"Use RTS/CTS hardware flow control. {help_shard}")
-        parser.add_argument(f"--{cli_shard}write-"
-                            f"timeout", default=__get_env__("WRITE_TIMEOUT", None, shard=shard),
+        parser.add_argument(f"--{cli_shard}write-timeout", default=__get_env__("WRITE_TIMEOUT", kwargs.get("write-timeout", None), shard=shard),
                             type=Decimal, help=f"The write timeout to use (seconds). {help_shard}")
-        parser.add_argument(f"--{cli_shard}dsrdtr", default=__get_env__("DSRDTR", False, shard=shard), type=bool,
+        parser.add_argument(f"--{cli_shard}dsrdtr", default=__get_env__("DSRDTR", kwargs.get("dsrdtr", False), shard=shard), type=bool,
                             choices=[True, False], help=f"Use DSR/DTR hardware flow control. {help_shard}")
         parser.add_argument(f"--{cli_shard}inter_byte_timeout",
-                            default=__get_env__("INTER_BYTE_TIMEOUT", None, shard=shard), type=Decimal,
+                            default=__get_env__("INTER_BYTE_TIMEOUT", kwargs.get("inter_byte_timeout", None), shard=shard), type=Decimal,
                             help=f"The inter byte timeout to use. Disabled by default. {help_shard}")
 
         if os.name == 'posix':
-            parser.add_argument(f"--{cli_shard}exclusive", default=__get_env__("EXCLUSIVE", None, shard=shard),
+            parser.add_argument(f"--{cli_shard}exclusive", default=__get_env__("EXCLUSIVE", kwargs.get("exclusive", None), shard=shard),
                                 type=bool, choices=[True, False],
                                 help=f"Open the serial port in exclusive mode. {help_shard}")
 
@@ -86,9 +93,10 @@ class SerialHelper:
         args = get_args(args, shard)
         kwargs = dict(
             port=args.port,
+            baudrate=args.baudrate,
             bytesize=args.bytesize,
             parity=cls.parity_map[args.parity],
-            stopbits=args.stopbits,
+            stopbits=cls.stopbit_map[args.stopbits],
             timeout=args.timeout,
             xonxoff=args.xonxoff,
             rtscts=args.rtscts,
